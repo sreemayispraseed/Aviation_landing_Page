c0a5e198c35accc1118e23a3408c14f2d42d70b0
sreemayispraseed <sreemayi99@gmail.com>
2022-10-13 12:30:18 +0530

    footer component added

---
+++ pages/Components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../../styles/Footer.module.css'
+import Image from 'next/image'
+
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
